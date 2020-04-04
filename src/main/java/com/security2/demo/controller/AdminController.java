05d1f32ab55c75ad556c62fe7d161585acecc5d7
Karthik Margam <Karthik.Margam@INFOVISIONLABS.COM>
2020-04-05 01:34:03 +0530

    first

---
+++ src/main/java/com/security2/demo/controller/AdminController.java
@@ -0,0 +1,36 @@
+package com.security2.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security2.demo.model.User;
+import com.security2.demo.repository.UserRepo;
+@RestController
+@RequestMapping("/secure/rest")
+public class AdminController {
+	@Autowired
+	UserRepo UserRepo;
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping("/admin/add")
+	public String addUserByAdmin(@RequestBody User user) {
+		String pwd = user.getPassword();
+		String encryptPwd = passwordEncoder.encode(pwd);
+		user.setPassword(encryptPwd);
+		UserRepo.save(user);
+		return "user added successfully...";
+	}
+	
+	@GetMapping("/login")
+	public String loo() {
+		return "login";
+	}
+}
